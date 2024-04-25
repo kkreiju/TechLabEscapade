9f9255a79b1300c3914d6c1db43388ab7cd5325e
Loreeeenzz <Blaise5255@gmail.com>
2024-04-26 03:29:27 +0800

    menu

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        TermsOfUse tou = new TermsOfUse();
+        tou.show();
+        tou.setLocationRelativeTo(null);
+    }
+    
+}
